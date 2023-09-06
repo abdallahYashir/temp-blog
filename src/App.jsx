2e8a727ab2807693769b55673429846dc899d198
Abdallah Ramsing <ext-abdallah.ramsing@skeepers.io>
2023-09-06 14:31:43 +0400

    Added basic structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import image from './assets/abdallah.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <h1 className="text-4xl font-bold text-center">
+        Hi, Welcome.
+      </h1>
+
+      <div className="flex flex-col items-center justify-center">
+        <img
+          src={image}
+          alt="Abdallah Yashir Ramsing"
+          width={300}
+          height={300}
+          className="rounded-full"
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+
+      <div className="text-center">
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          My name is Abdallah Yashir Ramsing.
         </p>
+        <p>My <a href="https://www.abdallahyashir.com/">Blog</a> is currently down.</p>
+        <p>Currently  🚧</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <div className="text-center">
+        <strong>You can meanwhile follow my work on:</strong>
+        <ul className="flex flex-row justify-center">
+          <li className="mx-2">
+            <a href="https://www.linkedin.com/in/abdallah-yashir-ramsing-51a20782/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+          </li>
+          <li className="mx-2">
+            <a href="https://twitter.com/AbdallahRamsing" target="_blank" rel="noopener noreferrer">Twitter</a>
+          </li>
+          <li className="mx-2">
+            <a href="https://abdallah-yashir.medium.com/" target="_blank" rel="noopener noreferrer">Medium</a>
+          </li>
+          <li className="mx-2">
+            <a href="https://www.youtube.com/channel/UCToIxwX31gz6ZHa1dOFshiA" target="_blank" rel="noopener noreferrer">YouTube</a>
+          </li>
+          <li className="mx-2">
+            <a href="https://www.quora.com/profile/Abdallah-Yashir" target="_blank" rel="noopener noreferrer">Quora</a>
+          </li>
+          <li className="mx-2">
+            <a href="https://dev.to/abdallahyashir" target="_blank" rel="noopener noreferrer">DevTo</a>
+          </li>
+        </ul>
+      </div>
     </>
   )
 }
